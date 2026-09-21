208e32f72b3af807f125001643635fd25ae2f183
Tiara <2522500065@mahasiswa.atmaluhur.ac.id>
2026-09-21 22:25:09 +0700

    push latihan1model

---
+++ pertemuan-02/model/latihan1Model.php
@@ -0,0 +1,12 @@
+<?php
+class latihan1Model
+{
+    public function getAllMhs()
+    {
+        return [
+            ['nama' => 'Tiara', 'nim' => '2522500065' ],
+            ['nama' => 'suci', 'nim' => '2522500057'];
+            ['nama' => 'prita', 'nim' => '2522500076']
+        ];
+    }
+}
